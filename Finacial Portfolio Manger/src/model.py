022efd155ccfa5ccfc1268305d31bf40f88378f4
aneeshcheriank <aneeshcheriank@gmail.com>
2026-05-04 09:48:58 +0200

    API keys updated, model updated

--- Finacial Portfolio Manger/src/model.py
+++ Finacial Portfolio Manger/src/model.py
@@ -7,12 +7,27 @@ from src.configuration import MODEL
 from src.environment import config_env
 
 # 4. Import LangChain now that the API key is securely set in os.environ
-from langchain_groq import ChatGroq
+# from langchain_groq import ChatGroq
+from langchain_deepseek import ChatDeepSeek
+import os
+
+# def get_llm():
+#     config_env()
+
+#     return ChatGroq(
+#         model=MODEL,
+#         temperature=0.0,
+#         api_key=os.environ.get("GROQ_API_KEY")
+#     )
 
 def get_llm():
     config_env()
 
-    return ChatGroq(
-        model=MODEL,
-        temperature=0.0
+    return ChatDeepSeek(
+        model="deepseek-v4-flash",
+        temperature=0.0,
+        api_key=os.environ.get("DEEPSEEK"),
+        extra_body={"thinking": { # to disable the reasoning (creates problems in formating tool)
+            "type": "disabled"
+        }} 
     )
